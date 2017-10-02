25ee749c57689899bc6427c250ca2f6512c0e570
James Montemagno <james.montemagno@gmail.com>
2017-10-02 15:25:10 +0200

    Add initial iOS/Android Code

---
+++ OSL/OSL/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace OSL
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
