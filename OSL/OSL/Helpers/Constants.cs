4fb571f4ec1ff6f1702c4f83f2881683ad6df017
James Montemagno <james.montemagno@gmail.com>
2017-10-05 16:06:49 -0700

    Add UWP

---
+++ OSL/OSL/Helpers/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSL.Helpers
+{
+    public static class Constants
+    {
+        public const string MobileCenteriOS = "903cd463-8b10-42b3-9320-b41519b9ca5d";
+        public const string MobileCenterAndroid = "a7e940e7-4b9e-4c7d-ae94-b09bfa843eb5";
+        public const string MobileCenterUWP = "31bacd41-05c4-4b23-b795-f3485eff8dc1";
+    }
+}
--- OSL/OSL/ViewModels/AboutViewModel.cs
+++ OSL/OSL/ViewModels/AboutViewModel.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms;
 
 namespace OSL
 {
-    public class AboutViewModel : BaseViewModel
+    public class AboutViewModel : ViewModelBase
     {
         public AboutViewModel()
         {
