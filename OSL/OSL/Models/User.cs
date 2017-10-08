f5c17de37e30aa8fa2f7c9590591b3d6762d9833
Samantha Gallagher <sjgllghr@gmail.com>
2017-10-07 20:44:16 -0700

    Add preliminary user model

---
+++ OSL/OSL/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace OSL.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string OId { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Long { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        //public string Country { get; set; }
+        public string PostalCode { get; set; }
+        public string State { get; set;}
+        public string Person_Name { get; set; }
+        public string Phone_Number { get; set; }
+        public string Status { get; set; }
+        public bool Verified { get; set; }
+    }
+}
