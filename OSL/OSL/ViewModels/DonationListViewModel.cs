5c447afd69014207b68e677926db730e174d9450
Samantha Gallagher <sjgllghr@gmail.com>
2017-11-11 14:25:35 -0800

    my donations list groupings

--- OSL/OSL/ViewModels/DonationListViewModel.cs
+++ OSL/OSL/ViewModels/DonationListViewModel.cs
@@ -17,11 +17,11 @@ namespace OSL
         {
             this.donationRepository = new DonationRepository();
             Title = "My Donations";
-            Items = new ObservableCollection<Donation>();
+            Items = new ObservableCollection<Group>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsAsync(), () => !IsBusy);
         }
 
-        public ObservableCollection<Donation> Items { get; set; }
+        public ObservableCollection<Group> Items { get; set; }
         public Command LoadItemsCommand { get; set; }
 
         async Task ExecuteLoadItemsAsync()
@@ -34,12 +34,29 @@ namespace OSL
             try
             {
                 Items.Clear();
+                var pending = new Group("Pending Pickup");
+                var completed = new Group("Completed");
+                var wasted = new Group("Wasted");
+                var listed = new Group("Listed");
+                Items.Clear();
                 var items = await donationRepository.GetDonationsByUserAsync();
 
                 foreach (var item in items)
                 {
-                    Items.Add(item);
+                    if (item.Status == DonationStatus.PendingPickup)
+                        pending.Add(item);
+                    else if (item.Status == DonationStatus.Completed)
+                        completed.Add(item);
+                    else if (item.Status == DonationStatus.Wasted)
+                        wasted.Add(item);
+                    else
+                        listed.Add(item);
+                    //Items.Add(item);
                 }
+                Items.Add(pending);
+                Items.Add(listed);
+                Items.Add(completed);
+                Items.Add(wasted);
             }
             catch (Exception ex)
             {
@@ -56,6 +73,16 @@ namespace OSL
         {
             get { return page; }
             set { SetProperty(ref page, value); }
+        }
+
+        public class Group : ObservableCollection<Donation>
+        {
+            public string Key { get; set; }
+
+            public Group(string key)
+            {
+                Key = key;
+            }
         }
     }
 }
